6704266de52694993ac27fab7f6276c1a7611428
Talha Demirkan <talhademirkn@gmail.com>
2022-10-27 16:30:59 +0300

    Seervice built

---
+++ lib/services/food_api.dart
@@ -0,0 +1,13 @@
+// import 'package:dio/dio.dart';
+// import 'package:veg_kitchen/models/food.dart';
+
+// class FoodApi {
+//   static const String _url =
+//       'https://api.spoonacular.com/recipes/complexSearch?apiKey=d33129e006ca499da3684e15240b01d5&diet=vegan&type=main course';
+
+//   static Future<List<Food>> fetchPokemonData() async {
+//     List<Food> _list = [];
+
+//     var response = await Dio().get(_url);
+//   }
+// }
