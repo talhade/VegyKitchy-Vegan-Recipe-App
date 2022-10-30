078520aa4c45293db5621e75c5a373e4e434dd63
Talha Demirkan <talhademirkn@gmail.com>
2022-10-30 16:13:14 +0300

    Started Creating Ingredients Tile

--- lib/widgets/food_list_tile.dart
+++ lib/widgets/food_list_tile.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:veg_kitchen/constants/paddings.dart';
 import 'package:veg_kitchen/models/food.dart';
+import 'package:veg_kitchen/screens/details_screen.dart';
 import 'package:veg_kitchen/theme/colors.dart';
 
 class FoodListTile extends StatelessWidget {
@@ -11,26 +12,33 @@ class FoodListTile extends StatelessWidget {
   Widget build(BuildContext context) {
     final double screenwidth = MediaQuery.of(context).size.width;
     final double screenheight = MediaQuery.of(context).size.height;
-    return Padding(
-      padding: EdgeInsets.symmetric(
-          horizontal: MyPaddings().defaultPadding,
-          vertical: MyPaddings().defaultPadding),
-      child: Container(
-        width: screenwidth,
-        height: screenheight / 4,
-        decoration: BoxDecoration(
-          color: MyColors().deco,
-          borderRadius: BorderRadius.circular(38),
-          boxShadow: [
-            _boxShadow(),
-          ],
-        ),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: [
-            _buildImage(screenwidth),
-            _titleField(screenwidth),
-          ],
+    return GestureDetector(
+      onTap: (() => Navigator.of(context).push(MaterialPageRoute(
+            builder: (context) => DetailsScreen(
+              food: food,
+            ),
+          ))),
+      child: Padding(
+        padding: EdgeInsets.symmetric(
+            horizontal: MyPaddings().defaultPadding,
+            vertical: MyPaddings().defaultPadding),
+        child: Container(
+          width: screenwidth,
+          height: screenheight / 4,
+          decoration: BoxDecoration(
+            color: MyColors().deco,
+            borderRadius: BorderRadius.circular(38),
+            boxShadow: [
+              _boxShadow(),
+            ],
+          ),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              _buildImage(screenwidth),
+              _titleField(screenwidth),
+            ],
+          ),
         ),
       ),
     );
@@ -62,9 +70,12 @@ class FoodListTile extends StatelessWidget {
       width: screenwidth * 0.3,
       child: ClipRRect(
         borderRadius: BorderRadius.circular(29),
-        child: Image.network(
-          food.image ?? '',
-          fit: BoxFit.cover,
+        child: Hero(
+          tag: food.id!,
+          child: Image.network(
+            food.image ?? '',
+            fit: BoxFit.cover,
+          ),
         ),
       ),
     );
