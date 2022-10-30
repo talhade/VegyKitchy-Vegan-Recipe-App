e0894b92132cf076764dff40180695e71f32f110
Talha Demirkan <talhademirkn@gmail.com>
2022-10-30 13:46:52 +0300

    Models Updated

--- lib/models/food.dart
+++ lib/models/food.dart
@@ -1,56 +1,37 @@
+// To parse this JSON data, do
+//
+//     final foodModel = foodModelFromJson(jsonString);
+
+import 'dart:convert';
+
+FoodModel foodModelFromJson(String str) => FoodModel.fromJson(json.decode(str));
+
+String foodModelToJson(FoodModel data) => json.encode(data.toJson());
+
 class FoodModel {
-  List<Results>? results;
-  int? offset;
-  int? number;
-  int? totalResults;
+  FoodModel({
+    this.id,
+    this.title,
+    this.image,
+    this.imageType,
+  });
 
-  FoodModel({this.results, this.offset, this.number, this.totalResults});
-
-  FoodModel.fromJson(Map<String, dynamic> json) {
-    if (json['results'] != null) {
-      results = <Results>[];
-      json['results'].forEach((v) {
-        results!.add(new Results.fromJson(v));
-      });
-    }
-    offset = json['offset'];
-    number = json['number'];
-    totalResults = json['totalResults'];
-  }
-
-  Map<String, dynamic> toJson() {
-    final Map<String, dynamic> data = new Map<String, dynamic>();
-    if (this.results != null) {
-      data['results'] = this.results!.map((v) => v.toJson()).toList();
-    }
-    data['offset'] = this.offset;
-    data['number'] = this.number;
-    data['totalResults'] = this.totalResults;
-    return data;
-  }
-}
-
-class Results {
   int? id;
   String? title;
   String? image;
   String? imageType;
 
-  Results({this.id, this.title, this.image, this.imageType});
+  factory FoodModel.fromJson(Map<String, dynamic> json) => FoodModel(
+        id: json["id"] == null ? null : json["id"],
+        title: json["title"] == null ? null : json["title"],
+        image: json["image"] == null ? null : json["image"],
+        imageType: json["imageType"] == null ? null : json["imageType"],
+      );
 
-  Results.fromJson(Map<String, dynamic> json) {
-    id = json['id'];
-    title = json['title'];
-    image = json['image'];
-    imageType = json['imageType'];
-  }
-
-  Map<String, dynamic> toJson() {
-    final Map<String, dynamic> data = new Map<String, dynamic>();
-    data['id'] = this.id;
-    data['title'] = this.title;
-    data['image'] = this.image;
-    data['imageType'] = this.imageType;
-    return data;
-  }
+  Map<String, dynamic> toJson() => {
+        "id": id == null ? null : id,
+        "title": title == null ? null : title,
+        "image": image == null ? null : image,
+        "imageType": imageType == null ? null : imageType,
+      };
 }
--- lib/models/menu_item.dart
+++ lib/models/menu_item.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:veg_kitchen/models/food.dart';
+import 'package:veg_kitchen/services/food_api.dart';
 
 class MenuItems {
   final String type;
@@ -6,15 +8,34 @@ class MenuItems {
   final String title;
   final String description;
   final IconData icon;
+  final Future<List<FoodModel>> fetchApi;
 
-  MenuItems(this.type, this.cardId, this.title, this.icon, this.description);
+  MenuItems(this.type, this.cardId, this.title, this.icon, this.description,
+      this.fetchApi);
 }
 
 List<MenuItems> menuItems = [
-  MenuItems('breakfast', 0, 'Breakfast', Icons.breakfast_dining_outlined,
-      'Have The Best Options Of Vegan Breakfasts.'),
-  MenuItems('main course', 1, 'Main Course', Icons.local_dining_outlined,
-      'Have The Best Options Of Vegan Main Courses.'),
-  MenuItems('snack', 2, 'Snack', Icons.foundation_outlined,
-      'Have The Best Options Of Vegan Snacks.'),
+  MenuItems(
+    'breakfast',
+    0,
+    'Breakfasts',
+    Icons.breakfast_dining_outlined,
+    'Have The Best Options Of Vegan Breakfasts.',
+    FoodApi.fetchBreakfasts(),
+  ),
+  MenuItems(
+      'main course',
+      1,
+      'Main Courses',
+      Icons.local_dining_outlined,
+      'Have The Best Options Of Vegan Main Courses.',
+      FoodApi.fetchMainCourses()),
+  MenuItems(
+    'snack',
+    2,
+    'Snacks',
+    Icons.foundation_outlined,
+    'Have The Best Options Of Vegan Snacks.',
+    FoodApi.fetchSnacks(),
+  ),
 ];
