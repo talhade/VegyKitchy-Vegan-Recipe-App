26c02767bd2ff0ee3fe9755c7cd893f01533d33b
Talha Demirkan <talhademirkn@gmail.com>
2022-11-25 16:27:55 +0300

    Updated

--- lib/secret/api_key.dart
+++ lib/secret/api_key.dart
@@ -1,3 +1,3 @@
 class ApiKey {
-  static const String apiKey = 'd33129e006ca499da3684e15240b01d5';
+  static String apiKey = 'd33129e006ca499da3684e15240b01d5';
 }
--- lib/services/food_api.dart
+++ lib/services/food_api.dart
@@ -1,15 +1,19 @@
 import 'package:dio/dio.dart';
 import 'package:veg_kitchen/models/food.dart';
+import 'package:veg_kitchen/secret/api_key.dart';
 
 class FoodApi {
-  static const String _urlMainCourse =
-      'https://api.spoonacular.com/recipes/complexSearch?apiKey=d33129e006ca499da3684e15240b01d5&diet=vegan&type=main course';
+  static final String _urlMainCourse =
+      'https://api.spoonacular.com/recipes/complexSearch?apiKey=${ApiKey.apiKey}&diet=vegan&type=main course';
 
-  static const String _urlBreakfast =
-      'https://api.spoonacular.com/recipes/complexSearch?apiKey=d33129e006ca499da3684e15240b01d5&diet=vegan&type=breakfast';
+  static final String _urlBreakfast =
+      'https://api.spoonacular.com/recipes/complexSearch?apiKey=${ApiKey.apiKey}&diet=vegan&type=breakfast';
 
-  static const String _urlSnack =
-      'https://api.spoonacular.com/recipes/complexSearch?apiKey=d33129e006ca499da3684e15240b01d5&diet=vegan&type=snack';
+  static final String _urlSnack =
+      'https://api.spoonacular.com/recipes/complexSearch?apiKey=${ApiKey.apiKey}&diet=vegan&type=snack';
+
+  static final String _urlRandom =
+      'https://api.spoonacular.com/recipes/complexSearch?apiKey=${ApiKey.apiKey}&diet=vegan&sort=random';
 
   static Future<List<FoodModel>> fetchMainCourses() async {
     List<FoodModel> _list = [];
@@ -41,5 +45,19 @@ class FoodApi {
       _list = data.map((e) => FoodModel.fromJson(e)).toList();
     }
     return _list;
+  }
+
+  static Future<List<FoodModel>> fetchRandomRecipe() async {
+    List<FoodModel> _list = [];
+    List<FoodModel> _firstItemList = [];
+    var response = await Dio().get(_urlRandom);
+    var data = response.data["results"];
+    if (data is List) {
+      _list = data.map((e) => FoodModel.fromJson(e)).toList();
+    }
+    var firstItem = _list[0];
+    _firstItemList.add(firstItem);
+
+    return _firstItemList;
   }
 }
