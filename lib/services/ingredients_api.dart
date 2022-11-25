f8638f0bc3d64a49fe7a9c65de321f94c648f46d
Talha Demirkan <talhademirkn@gmail.com>
2022-11-25 16:36:10 +0300

    Updated service

--- lib/services/ingredients_api.dart
+++ lib/services/ingredients_api.dart
@@ -1,10 +1,11 @@
 import 'package:dio/dio.dart';
 import 'package:veg_kitchen/models/ingredients_model.dart';
+import 'package:veg_kitchen/secret/api_key.dart';
 
 class IngredientsApi {
   static Future<List<IngredientsModel>> fetchIngredientsById(int id) async {
     late String _url =
-        'https://api.spoonacular.com/recipes/${id}/ingredientWidget.json?apiKey=d33129e006ca499da3684e15240b01d5';
+        'https://api.spoonacular.com/recipes/${id}/ingredientWidget.json?apiKey=${ApiKey.apiKey}';
     List<IngredientsModel> _list = [];
     var response = await Dio().get(_url);
     var data = response.data["ingredients"];
